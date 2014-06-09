69d85be63b680c54423df9cdc9bf3eab879e39b1
Bill ROUSSEAU <birousse@e1r11p6.42.fr>
2014-06-09 17:53:43 +0200

    add index.php file(s)

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8-1" />
+<!--[if lt IE 9]> <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script> <![endif]-->
+
+<link rel="stylesheet" href="css/design.css" />
+
+<title>
+Minishop 42
+</title>
+</head>
+
+<body>
+
+<?php
+// Inclusion de l'entête de page
+include("pages/header.php");
+// Inclusion du menu principal de la page
+include("pages/mainmenu.php");
+
+// Inclusion du contenu de la page
+include("pages/content.php");
+
+// Inclusion du pied de page
+include("pages/footer.php");
+?>
+
+</body>
+</html>
+
